88d670403659cd00f287a5d50a6fedaa226d8b4a
Sathy77 <sathy754@gmail.com>
2023-10-15 18:00:16 +0600

    login

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from userauth.views import RegisterAPI
+from userauth.views import RegisterAPI, LoginAPI
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
 ]
--- userauth/views.py
+++ userauth/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions             #permissions only for login
 from rest_framework.response import Response
 from knox.models import AuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer #only for login
+from knox.views import LoginView as KnoxLoginView                    #only for login
+from django.contrib.auth import login                                #only for login
+
 from userauth.serializers import RegisterSerializer
 
 # Create your views here.
@@ -16,4 +20,14 @@ class RegisterAPI(generics.GenericAPIView):
             'user': request.data['username'],
             'token': AuthToken.objects.create(user)[1]
         }, status=status.HTTP_201_CREATED)
+    
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
 
